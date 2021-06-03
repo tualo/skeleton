0983f2fc7c7c137e61180aa7038f01c83fb4a9f3
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2021-06-03 11:07:00 +0200

    add hints

--- src/Routes/Route.php
+++ src/Routes/Route.php
@@ -4,9 +4,20 @@ use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
 
+/*
+    >>> GET / POST  URI
+    >>> SERVER
 
+    >>> PHP 
+        >>> Middlewares
+            >> 1-n
+            >> ROUTEN MW
+                >>> ROUTE (BASIS URI)
+
+*/
 class Route implements IRoute{
     public static function register(){
+
         BasicRoute::add('/skeleton/(?P<file>[\/.\w\d\-\_]+)'.'.js',function($matches){
             if (file_exists( dirname(__DIR__).'/js/'.$matches['file'].'.js') ){
                 App::etagFile( dirname(__DIR__).'/js/'.$matches['file'].'.js' , true);
