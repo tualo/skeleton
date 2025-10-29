5976c1c8f80baec25c81b6f1ac6294ad7109806d
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-10-29 22:30:01 +0100

    Version bump to 1.0.5

--- src/Routes/Route.php
+++ src/Routes/Route.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Tualo\Office\Skeleton\Routes;
+
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
@@ -15,26 +17,27 @@ use Tualo\Office\Basic\IRoute;
                 >>> ROUTE (BASIS URI)
 
 */
-class Route implements IRoute{
-    public static function register(){
 
-        BasicRoute::add('/skeleton/(?P<file>[\/.\w\d\-\_]+)'.'.js',function($matches){
-            if (file_exists( dirname(__DIR__).'/js/'.$matches['file'].'.js') ){
-                App::etagFile( dirname(__DIR__).'/js/'.$matches['file'].'.js' , true);
+class Route extends \Tualo\Office\Basic\RouteWrapper
+{
+    public static function register()
+    {
+
+        BasicRoute::add('/skeleton/(?P<file>[\/.\w\d\-\_]+)' . '.js', function ($matches) {
+            if (file_exists(dirname(__DIR__) . '/js/' . $matches['file'] . '.js')) {
+                App::etagFile(dirname(__DIR__) . '/js/' . $matches['file'] . '.js', true);
             }
-        },array('get'),false);
+        }, array('get'), false);
 
 
-        BasicRoute::add('/skeleton/hello',function($matches){
-            App::result('msg', 'hello world' );
-            App::result('success', true );
+        BasicRoute::add('/skeleton/hello', function ($matches) {
+            App::result('msg', 'hello world');
+            App::result('success', true);
 
-            App::result('skeleton_time_loggedIn',$_SESSION['skeleton_time_loggedIn']);
-            App::result('skeleton_time',$_SESSION['skeleton_time']);
-            
+            App::result('skeleton_time_loggedIn', $_SESSION['skeleton_time_loggedIn']);
+            App::result('skeleton_time', $_SESSION['skeleton_time']);
+
             App::contenttype('application/json');
-        },array('get'),false);
-
-
+        }, array('get'), false);
     }
 }
